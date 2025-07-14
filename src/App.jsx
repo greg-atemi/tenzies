11dc9cd5cf429b19c1a9b2b0f2a889351b53be47
gregatemi <greg.atemi@systechafrica.com>
2025-07-14 07:44:49 +0300

    Added nanoid for Die element keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from "react"
+import { nanoid } from "nanoid";
 import Die from "./components/Die";
 
 export default function App() {
     const [dice, setDice] = React.useState(generateAllNewDice()); // Initialize state with random numbers
 
     function generateAllNewDice() {
-      return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1)
+      return Array.from({ length: 10 }, () => ({
+        value: Math.floor(Math.random() * 6) + 1, // Random number between 1 and 6
+        isHeld: false, // Default to false
+        id: nanoid() // Unique ID for each die
+      }));
     }
 
     function rollDice() {
         setDice(generateAllNewDice());
     }
 
-    const diceElements = dice.map(num => <Die value={num} />);
+    // const diceElements = dice.map(num => <Die value={num} />);
+
+    const diceElements = dice.map(dieObj => (
+      <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+    ));
 
     return (
         <>
